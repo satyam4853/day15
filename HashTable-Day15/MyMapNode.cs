3b13e193f03c400e8eb423d938f7cebafb78154d
Your Name <you@example.com>
2021-08-17 18:12:25 +0530

    feat: find frequency of words in sentence like To be or not to be

---
+++ HashTable-Day15/MyMapNode.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable_Day15
+{
+    class MyMapNode<K, V>
+    {
+        public struct KeyValue<k, v>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        /// <summary>
+        /// Gets the index position from haschcode.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        /// <summary>
+        /// Gets the specified key from a linked list.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+            return default(V);
+        }
+        /// <summary>
+        /// Adds the specified key and value in the linked list at the specified index.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            linkedList.AddLast(item);
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+        /// <summary>
+        /// Removes the specified key from the linked list.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+        /// <summary>
+        /// Creates a new linked list on the given index
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        /// <summary>
+        /// Displays the key and value inside the linked list
+        /// </summary>
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+        /// <summary>
+        /// Gets the freqency of each word in the sentence.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void GetFreq(V value)
+        {
+            int freq = 0;
+            foreach (LinkedList<KeyValue<K, V>> list in items)
+            {
+                if (list == null)
+                    continue;
+                foreach (KeyValue<K, V> obj in list)
+                {
+                    if (obj.Equals(null))
+                        continue;
+                    if (obj.Value.Equals(value))
+                        freq++;
+                }
+            }
+            Console.WriteLine("Frequency of {0} is {1}", value, freq);
+
+        }
+    }
+}
--- HashTable-Day15/Program.cs
+++ HashTable-Day15/Program.cs
@@ -7,6 +7,15 @@ namespace HashTable_Day15
         static void Main(string[] args)
         {
             Console.WriteLine("---------------Welcome to Hash Table Prolem---------------");
+            MyMapNode<string, string> mapNode = new MyMapNode<string, string>(5);
+            Console.WriteLine("Adding KeyValue pair");
+            mapNode.Add("0", "to");
+            mapNode.Add("1", "be");
+            mapNode.Add("2", "or");
+            mapNode.Add("3", "to");
+            mapNode.Add("4", "be");
+            mapNode.Add("5", "not");
+            mapNode.GetFreq("to");
         }
     }
 }
